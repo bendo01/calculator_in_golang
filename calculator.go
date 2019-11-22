896bb1487381eedda3ac4ab533a768fa8254bd1b
Benny L.E.p <bendo01@gmail.com>
2019-11-22 15:01:08 +0800

    menambahkan fungsi

--- calculator.go
+++ calculator.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+func repeatHelper() {
+	fmt.Println("Tekan huruf Y jika ingin melakukan perhitungan")
+	fmt.Println("Tekan huruf T jika ingin keluar dari applikasi")
+	fmt.Println("Apakah Anda Ingin Melakukan Perhitungan ?")
+}
+
+func inputHelper() {
+	fmt.Println("Perhitungan Dengan Format operand1 spasi operator spasi operand2")
+	fmt.Println("contoh: 1 + 1")
+}
+
 func main() {
 	fmt.Println("Masih Malas Ka coding")
 }
